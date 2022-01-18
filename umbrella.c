c16632afce2480d7bf162635bfda0cfae74fe4b6
Marcello Belanda <88407713+MarcelloBB@users.noreply.github.com>
2022-01-18 18:42:12 -0300

    Update umbrella.c

--- umbrella.c
+++ umbrella.c
@@ -7,31 +7,31 @@ int main()
 	printf("\n%sBOLD%s\n", BOLD_ON, BOLD_OFF);
 	
 	// STD FORE
-	printf("\n%sWHITE%s\n", WHITE_FORE, ENDC);
-	printf("\n%sBLACK%s\n", BLACK_FORE, ENDC);
-	printf("\n%sYELLOW%s\n", YELLOW_FORE, ENDC);
+	printf("\n%sWHITE%s ", WHITE_FORE, ENDC);
+	printf("\n%sBLACK%s ", BLACK_FORE, ENDC);
+	printf("\n%sYELLOW%s ", YELLOW_FORE, ENDC);
 	printf("\n%sRED%s\n", RED_FORE, ENDC);
-	printf("\n%sMAGENTA%s\n", MAGENTA_FORE, ENDC);
-	printf("\n%sBLUE%s\n", BLUE_FORE, ENDC);
-	printf("\n%sCYAN%s\n", CYAN_FORE, ENDC);
+	printf("\n%sMAGENTA%s ", MAGENTA_FORE, ENDC);
+	printf("\n%sBLUE%s ", BLUE_FORE, ENDC);
+	printf("\n%sCYAN%s", CYAN_FORE, ENDC);
 	
 	// BRIGHT FORE
-	printf("\n%sBRIGHT_WHITE%s\n", BRIGHT_WHITE_FORE, ENDC);
-	printf("\n%sBRIGHT_BLACK%s\n", BRIGHT_BLACK_FORE, ENDC);
-	printf("\n%sBRIGHT_YELLOW%s\n", BRIGHT_YELLOW_FORE, ENDC);
-	printf("\n%sBRIGHT_RED%s\n", BRIGHT_RED_FORE, ENDC);
-	printf("\n%sBRIGHT_MAGENTA%s\n", BRIGHT_MAGENTA_FORE, ENDC);
-	printf("\n%sBRIGHT_BLUE%s\n", BRIGHT_BLUE_FORE, ENDC);
-	printf("\n%sBRIGHT_CYAN%s\n", BRIGHT_CYAN_FORE, ENDC);
+	printf("\n%sBRIGHT_WHITE%s ", BRIGHT_WHITE_FORE, ENDC);
+	printf("\n%sBRIGHT_BLACK%s ", BRIGHT_BLACK_FORE, ENDC);
+	printf("\n%sBRIGHT_YELLOW%s ", BRIGHT_YELLOW_FORE, ENDC);
+	printf("\n%sBRIGHT_RED%s ", BRIGHT_RED_FORE, ENDC);
+	printf("\n%sBRIGHT_MAGENTA%s ", BRIGHT_MAGENTA_FORE, ENDC);
+	printf("\n%sBRIGHT_BLUE%s ", BRIGHT_BLUE_FORE, ENDC);
+	printf("\n%sBRIGHT_CYAN%s ", BRIGHT_CYAN_FORE, ENDC);
 	
 	// STD BACK
-	printf("\n%sBACK_WHITE%s\n", WHITE_BACK, ENDC);
-	printf("\n%sBACK_BLACK%s\n", BLACK_BACK, ENDC);
-	printf("\n%sBACK_YELLOW%s\n", YELLOW_BACK, ENDC);
-	printf("\n%sBACK_RED%s\n", RED_BACK, ENDC);
-	printf("\n%sBACK_MAGENTA%s\n", MAGENTA_BACK, ENDC);
-	printf("\n%sBACK_BLUE%s\n", BLUE_BACK, ENDC);
-	printf("\n%sBACK_CYAN%s\n", CYAN_BACK, ENDC);
+	printf("\n%sBACK_WHITE%s ", WHITE_BACK, ENDC);
+	printf("\n%sBACK_BLACK%s ", BLACK_BACK, ENDC);
+	printf("\n%sBACK_YELLOW%s ", YELLOW_BACK, ENDC);
+	printf("\n%sBACK_RED%s ", RED_BACK, ENDC);
+	printf("\n%sBACK_MAGENTA%s ", MAGENTA_BACK, ENDC);
+	printf("\n%sBACK_BLUE%s ", BLUE_BACK, ENDC);
+	printf("\n%sBACK_CYAN%s ", CYAN_BACK, ENDC);
 	
 	return 0;
 }
