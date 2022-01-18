da39975bf0f60f3652191d375242dc2a378c6db8
Marcello Belanda <88407713+MarcelloBB@users.noreply.github.com>
2022-01-18 17:51:48 -0300

    Update umbrella.c

--- umbrella.c
+++ umbrella.c
@@ -7,20 +7,20 @@ int main()
 	printf("\n%sBOLD%s\n", BOLD_ON, BOLD_OFF);
 	
 	// STD FORE
-	printf("\n%sWHITE\n", WHITE_FORE);
-	printf("\n%sBLACK\n", BLACK_FORE);
-	printf("\n%sYELLOW\n", YELLOW_FORE);
-	printf("\n%sRED\n", RED_FORE);
-	printf("\n%sMAGENTA\n", MAGENTA_FORE);
+	printf("\n%sWHITE%s\n", WHITE_FORE, ENDC);
+	printf("\n%sBLACK%s\n", BLACK_FORE, ENDC);
+	printf("\n%sYELLOW%s\n", YELLOW_FORE, ENDC);
+	printf("\n%sRED%s\n", RED_FORE, ENDC);
+	printf("\n%sMAGENTA%s\n", MAGENTA_FORE, ENDC);
 	printf("\n%sBLUE%s\n", BLUE_FORE, ENDC);
 	printf("\n%sCYAN%s\n", CYAN_FORE, ENDC);
 	
 	// BRIGHT FORE
-	printf("\n%sBRIGHT_WHITE\n", BRIGHT_WHITE_FORE);
-	printf("\n%sBRIGHT_BLACK\n", BRIGHT_BLACK_FORE);
-	printf("\n%sBRIGHT_YELLOW\n", BRIGHT_YELLOW_FORE);
-	printf("\n%sBRIGHT_RED\n", BRIGHT_RED_FORE);
-	printf("\n%sBRIGHT_MAGENTA\n", BRIGHT_MAGENTA_FORE);
+	printf("\n%sBRIGHT_WHITE%s\n", BRIGHT_WHITE_FORE, ENDC);
+	printf("\n%sBRIGHT_BLACK%s\n", BRIGHT_BLACK_FORE, ENDC);
+	printf("\n%sBRIGHT_YELLOW%s\n", BRIGHT_YELLOW_FORE, ENDC);
+	printf("\n%sBRIGHT_RED%s\n", BRIGHT_RED_FORE, ENDC);
+	printf("\n%sBRIGHT_MAGENTA%s\n", BRIGHT_MAGENTA_FORE, ENDC);
 	printf("\n%sBRIGHT_BLUE%s\n", BRIGHT_BLUE_FORE, ENDC);
 	printf("\n%sBRIGHT_CYAN%s\n", BRIGHT_CYAN_FORE, ENDC);
 	
