b14250f705d0581f7a9815ac1dc1dac8ab75a889
MarcelloBB <marcellobelanda@gmail.com>
2022-01-18 15:56:22 -0300

    ADD umbrella.c to test umbrella.h

---
+++ umbrella.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "umbrella.h"
+
+int main()
+{
+	printf("\n%sYELLOW FORE - %sUNDERLINED%s - %sBOLD%s\n", YELLOW_FORE, UNDERLINE_ON, UNDERLINE_OFF, BOLD_ON, BOLD_OFF);
+	return 0;
+}
