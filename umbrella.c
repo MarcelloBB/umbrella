90bc93cd0b771522be291f54c7389646cfbbe816
Marcello Belanda <88407713+MarcelloBB@users.noreply.github.com>
2022-01-18 16:38:40 -0300

    Update umbrella.c

--- umbrella.c
+++ umbrella.c
@@ -3,13 +3,29 @@
 
 int main()
 {
+	//  UNDERLINED
+	printf("\n%sUNDERLINED%s\n", UNDERLINE_ON, UNDERLINE_OFF);
+
+	// BOLD
+	printf("\n%sUNDERLINED%s\n", UNDERLINE_ON, UNDERLINE_OFF);
+	
+	// STD FORE
+	printf("\n%sWHITE\n", WHITE_FORE);
+	printf("\n%sBLACK\n", BLACK_FORE);
 	printf("\n%sYELLOW\n", YELLOW_FORE);
 	printf("\n%sRED\n", RED_FORE);
 	printf("\n%sMAGENTA\n", MAGENTA_FORE);
 	printf("\n%sBLUE%s\n", BLUE_FORE, ENDC);
 	printf("\n%sCYAN%s\n", CYAN_FORE, ENDC);
-	printf("\n%sUNDERLINED%s\n", UNDERLINE_ON, UNDERLINE_OFF);
-	printf("\n%sBOLD%s\n", BOLD_ON, BOLD_OFF);
+	
+	// BRIGHT FORE
+	printf("\n%sBRIGHT_WHITE\n", BRIGHT_WHITE_FORE);
+	printf("\n%sBRIGHT_BLACK\n", BRIGHT_BLACK_FORE);
+	printf("\n%sBRIGHT_YELLOW\n", BRIGHT_YELLOW_FORE);
+	printf("\n%sBRIGHT_RED\n", BRIGHT_RED_FORE);
+	printf("\n%sBRIGHT_MAGENTA\n", BRIGHT_MAGENTA_FORE);
+	printf("\n%sBRIGHT_BLUE%s\n", BRIGHT_BLUE_FORE, ENDC);
+	printf("\n%sBRIGHT_CYAN%s\n", BRIGHT_CYAN_FORE, ENDC);
 	
 	return 0;
 }
