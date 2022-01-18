5fc2193a9dc51e0321590c539f1a15e74ac0f389
Marcello Belanda <88407713+MarcelloBB@users.noreply.github.com>
2022-01-18 11:39:01 -0300

    Create umbrella.h

---
+++ umbrella.h
@@ -0,0 +1,27 @@
+#ifndef       UMBRELLA_H
+#define       UMBRELLA_H
+
+/* ====================================================
+ *   EFFECTS
+ * ==================================================== */
+ 
+/* UNDERLINE (ON/OFF) */
+#define       UNDERLINE_ON      "\033[4m"
+#define       UNDERLINE_OFF     "\033[24m"
+
+/* BOLD (ON/OFF) */
+#define       BOLD_ON           "\033[1m"
+#define       BOLD_OFF          "\033[21m"
+
+/* ====================================================
+ *   FOREGROUND
+ * ==================================================== */
+#define       RED_FORE          "\033[31m"
+#define       GREEN_FORE        "\033[32m"
+#define       YELLOW_FORE       "\033[33m"
+#define       BLUE_FORE         "\033[34m"
+#define       MAGENTA_FORE      "\033[35m"
+#define       CYAN_FORE         "\033[36m"
+#define       WHITE_FORE        "\033[37m"
+
+#endif
