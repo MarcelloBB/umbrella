8f6fe39aad6fae34e4288437c09b3025acfa49b7
Marcello Belanda <88407713+MarcelloBB@users.noreply.github.com>
2022-01-18 16:03:10 -0300

    Update umbrella.c

--- umbrella.c
+++ umbrella.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-	printf("\n%sYELLOW FORE - %sUNDERLINED%s - %sBOLD%s\n", YELLOW_FORE, UNDERLINE_ON, UNDERLINE_OFF, BOLD_ON, BOLD_OFF);
+	printf("\n%sYELLOW\n", YELLOW_FORE);
+	printf("\n%sRED\n", RED_FORE);
+	printf("\n%sMAGENTA\n", MAGENTA_FORE);
+	printf("\n%sBLUE\n", BLUE_FORE);
+	printf("\n%sCYAN\n", CYAN_FORE);
+	printf("\n%sUNDERLINED%s\n", UNDERLINE_ON, UNDERLINE_OFF);
+	printf("\n%sBOLD%s\n", BOLD_ON, BOLD_OFF);
+	
 	return 0;
 }
